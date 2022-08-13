f782182da96398369bdc0ad29ea5c1c7614988d2
Lucas Moura Gonçalves <92659220+LucasMZ@users.noreply.github.com>
2022-08-13 18:04:25 -0300

    Data e componente

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <componenteAgua/>
+</template>
+<script>
+import componenteAgua from './componenteAgua.vue'
+export default {
+name: 'App',
+components: {
+  componenteAgua
+  }
+}
+</script>
+
+
+
+
+
---
+++ componenteAgua.vue
@@ -0,0 +1,21 @@
+
+<template>
+<div>
+    <input v-model="nome"/>
+   <p> Ola eu sou o {{nome}}</p>
+  
+</div>
+</template>
+<script>
+export default{
+     name: 'componenteAgua',
+     data() {
+        return {
+            nome: ''
+        }
+     }
+     }
+
+</script>
+
+
